2f4ba769fcc7106fb3c5e616edc6de0ce55efe43
massumedh <78467589+massumedh@users.noreply.github.com>
2021-10-16 01:42:36 +0330

    add proje

---
+++ MarketPlace/MarketPlace.DAL.EF/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MarketPlace.DAL.EF
+{
+    public class Class1
+    {
+    }
+}
---
+++ MarketPlace/MarketPlace.Domain.Entites/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MarketPlace.Domain.Entites
+{
+    public class Class1
+    {
+    }
+}
