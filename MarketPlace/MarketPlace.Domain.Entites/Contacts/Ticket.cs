25a83b2daa62c91107695e5819de698367f1489b
massumedh <78467589+massumedh@users.noreply.github.com>
2021-10-31 02:42:33 +0330

    ticket

---
+++ MarketPlace/MarketPlace.Domain.Entites/Contacts/Ticket.cs
@@ -0,0 +1,67 @@
+﻿using MarketPlace.Domain.Entites.Account;
+using MarketPlace.Domain.Entites.Common;
+using System.ComponentModel.DataAnnotations;
+namespace MarketPlace.Domain.Entites.Contacts
+{
+    public class Ticket : BaseEntity
+    {
+        #region properties
+        public long OwnerId { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Title { get; set; }
+
+        [Display(Name = "بخش مورد نظر")]
+        public TicketSection  TicketSection{ get; set; }
+
+        [Display(Name = "اولویت")]
+        public TicketPriority TicketPriority { get; set; }
+
+        [Display(Name = "وضعیت تیکت")]
+        public TicketState TicketState { get; set; }
+
+        public bool IsReadByOwner { get; set; }
+
+        public bool IsReadByAdmin { get; set; }
+        #endregion
+
+        #region relation
+        public User Owner { get; set; }
+        #endregion
+
+    }
+    #region enum
+    public enum TicketSection
+    {
+        [Display(Name = "پشتیبانی")]
+        support,
+        [Display(Name = "فنی")]
+        Technical,
+        [Display(Name = "آموزشی")]
+        Academic
+    }
+
+    public enum TicketPriority
+    {
+        [Display(Name = "کم")]
+        Low,
+        [Display(Name = "متوسط")]
+        Medium,
+        [Display(Name = "زیاد")]
+        High
+    }
+
+    public enum TicketState
+    {
+        [Display(Name = "در حال بررسی")]
+        UnderProgress,
+        [Display(Name = "پاسخ داده شده")]
+        Answered,
+        [Display(Name = "بسته شده")]
+        Closed
+    }
+    #endregion
+
+}
---
+++ MarketPlace/MarketPlace.Domain.Entites/Contacts/TicketMessage.cs
@@ -0,0 +1,23 @@
+﻿using MarketPlace.Domain.Entites.Account;
+using MarketPlace.Domain.Entites.Common;
+using System.ComponentModel.DataAnnotations;
+namespace MarketPlace.Domain.Entites.Contacts
+{
+    public class TicketMessage : BaseEntity
+    {
+        #region properties
+
+        public long TicketId { get; set; }
+        public long SenderId { get; set; }
+
+        [Display(Name = "متن پیام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Text { get; set; }
+        #endregion
+
+        #region relation
+        public Ticket Ticket { get; set; }
+        public User Sender { get; set; }
+        #endregion
+    }
+}
